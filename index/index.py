f0ebaaec72c2da9c38d90d9dbb96f9f4d84d4bec
Brunodesouzadosanjos <brunodesouzadosanjos21@gmail.com>
2025-04-08 19:04:48 -0300

    interface basica

---
+++ index/index.py
@@ -0,0 +1,21 @@
+import os
+import time
+lista = []
+
+
+while True == True:
+    opcao = input('''
+    *******************************************
+    *          Lista de compras               * 
+    *   [1]Adicionar                          * 
+    *   [2]Apagar                             * 
+    *   [3]Listar                             *
+    *   [4]Sair                               *                                       
+    *******************************************
+    :   ''')
+    if opcao == '4':
+        print('Obrigado, até mais!')
+        time.sleep(3)
+        break
+os.system('cls')
+
