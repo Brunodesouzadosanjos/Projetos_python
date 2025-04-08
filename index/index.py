fcda34bcdfb853112a3a023b6bfd6f38538aee1a
Brunodesouzadosanjos <brunodesouzadosanjos21@gmail.com>
2025-04-08 19:27:43 -0300

    adicionando funcoes

--- index/index.py
+++ index/index.py
@@ -1,6 +1,20 @@
 import os
 import time
 lista = []
+def adicionar():
+    adicionar_produto = input('Produto: ')
+    lista.append(adicionar_produto)
+def apagar():
+    try:
+        apagar_produto = int(input("Produto que sera apagado, digite o indice: "))
+        if apagar_produto in lista:
+            del lista[apagar_produto]
+    except:
+        print('opção incorreta, por favor apenas numeros')
+def listar():
+    for produto in enumerate(lista):
+        indice,nome = produto
+        print(indice,nome)
 
 
 while True == True:
@@ -17,5 +31,23 @@ while True == True:
         print('Obrigado, até mais!')
         time.sleep(3)
         break
+    elif opcao == '1':
+        print('Adicionar')
+        adicionar()
+        time.sleep(2)
+        os.system('cls')
+    elif opcao == "2":
+        print('Apagar')
+        apagar()
+        time.sleep(2)
+        os.system('cls')
+    elif opcao == "3":
+        print('Listar')
+        listar()
+    else:
+        print("Digite apenas as opções informada")
+        time.sleep(2)
+        os.system('cls')
+    
 os.system('cls')
 
